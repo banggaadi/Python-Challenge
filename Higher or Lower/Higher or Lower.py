502a216cc8929448e95483b85bd4bcb5c2f0dbe0
Bangga Adi <55023515+banggaadi@users.noreply.github.com>
2023-12-21 03:15:21 +0700

    Add files via upload

--- Higher or Lower/Higher or Lower.py
+++ Higher or Lower/Higher or Lower.py
@@ -2,39 +2,55 @@ from game_data import data
 import random
 
 final_score = 0
+game_should_continue = True
+
+
+def get_option():
+    return random.choice(data)
 
 
 def check(answer, A, B):
     global final_score
+    global game_should_continue
     if answer == "A" and A["follower_count"] > B["follower_count"]:
         print("You're Right")
         final_score += 1
         print("Your Score:", final_score)
         print("\n")
-        play()
+        return A
     elif answer == "B" and A["follower_count"] < B["follower_count"]:
         print("You're Right")
         final_score += 1
         print("Your Score:", final_score)
         print("\n")
-        play()
+        return B
     else:
         print("You are wrong, this is your final score:", final_score)
+        game_should_continue = False
+        return None
 
 
 def play():
-    first_option = random.choice(data)
-    second_option = random.choice(data)
+    global game_should_continue
+    global final_score
 
-    print("Compare\nA= " + first_option["name"] + ", a " + first_option["description"] + ", from " + first_option[
-        "country"] + "\n VS \nB= " + second_option["name"] + ", a " + second_option["description"] + ", from " +
-          second_option["country"])
-    print("Who has more followers?")
+    first_option = get_option()
+    second_option = get_option()
 
-    answer = input()
-    check(answer, first_option, second_option)
+    while game_should_continue:
+        print("Compare\nA= " + first_option["name"] + ", a " + first_option["description"] + ", from " +
+              first_option["country"] + "\n VS \nB= " + second_option["name"] + ", a " + second_option["description"] +
+              ", from " + second_option["country"])
+        print("Who has more followers?")
+
+        answer = input()
+        new_first_option = check(answer, first_option, second_option)
+
+        if game_should_continue:
+            if new_first_option:
+                first_option = new_first_option
+                second_option = get_option()
 
 
 play()
-
 
